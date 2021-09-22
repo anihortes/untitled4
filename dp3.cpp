7298c56e6ce5ff841e0073bba200960b30cd2486
Adam Owen <ahowenjr@alaska.edu>
2021-09-21 22:31:58 -0800

    refined comments

--- dp3.cpp
+++ dp3.cpp
@@ -1,19 +1,20 @@
-// dp3.cpp  SKELETON
-// Glenn G. Chappell
-// 2021-09-16
-//
-// For CS 311 Fall 2021
-// Source for Project 3 functions
+// dp3.cpp
+// A. Harrison Owen
+// Started: 2021-09-17
+// Last edit: 2021-09-21
+// dp3 (i do not know what dp3 means)
+//      function didItThrow checks if function throws an error
+//      function gcd finds the greatest common factor of two numbers
 
 #include "dp3.h"       // For Project 3 prototypes & templates
 
 #include <functional>
 using std::function;
 
-
+// error handling function :
+// takes a function or object and if it throws an error, threw = true
 void didItThrow(const function<void()> & ff,
-                bool & threw)
-{
+                bool & threw) {
     try {
         ff();
     }
@@ -24,17 +25,14 @@ void didItThrow(const function<void()> & ff,
     threw = false;
 }
 
-
-int gcd(int a,
-        int b)
-{
+// finds the greatest common factor between two ints using recursion
+int gcd(int a, int b) {
+    // base case
     if(a == 0) return b;
+    // recursive cases
     else if(a > b)
         return gcd(b,a);
     else{
         return gcd(b%a,a);
     }
-    return 42;  // Dummy return
-    // TODO: WRITE THIS!!!
 }
-
--- dp3.h
+++ dp3.h
@@ -1,9 +1,11 @@
-// dp3.h  SKELETON
-// Glenn G. Chappell
-// 2021-09-16
-//
-// For CS 311 Fall 2021
-// Header for Project 3 functions
+// dp3.h
+// A. Harrison Owen
+// Started: 2021-09-17
+// Last edit: 2021-09-21
+// dp3 header (i do not know what dp3 means)
+//      function lookup returns data in array
+//      function uniqueCount returns number of unique items in array
+
 
 #ifndef FILE_DP3_H_INCLUDED
 #define FILE_DP3_H_INCLUDED
@@ -16,27 +18,31 @@
 using std::cout;
 using std::endl;
 
-
+// linked list template
+// is given pointer in array and an index to look for
 template <typename ValueType>
 ValueType lookup(const LLNode<ValueType> * head,
-                 std::size_t index)
-{
+                 std::size_t index) {
     auto ptr = head;
-    auto data = head;
-    auto count=0;
-
+    auto count = 0;
+    // checks if the index is not in range
     if(index < 0)
         throw std::out_of_range("bad index");
 
+    // get size of the array
     while(ptr != nullptr){
         count++;
         ptr = ptr->_next;
     }
+
+// with the size known, checks again if the index is not in range
     if(index >= count)
         throw std::out_of_range("bad index");
 
-    ptr = head;
-    count = 0;
+    ptr = head; // return iter to point to head
+    count = 0;  // reset count now that we know index is safe
+
+    // iterate through array, return data iter points to
     while(ptr != nullptr){
         if(count == index) {
             return ptr->_data;
@@ -52,19 +58,21 @@ ValueType lookup(const LLNode<ValueType> * head,
 void didItThrow(const std::function<void()> & ff,
                 bool & threw);
 
-
+// returns number of unique items in an array
+// first and last points to indices in an array
 template <typename RAIter>
 std::size_t uniqueCount(RAIter & first,
-                        RAIter & last)
-{
-    auto count = 0;
-    auto iterOne = first;
-    auto iterTwo = first;
-    auto size = last - first;
-    if(size == 0) return 0;
-    if(size == 1) return 1;
+                        RAIter & last) {
+    std::size_t count;
+    // check for smallest array scenarios
+    if((last - first) == 0) return 0;
+    if((last - first) == 1) return 1;
+
+    // after std::sort, std::unique removes all repeated items
     std::sort(first, last);
+    // lst points to the last item not removed
     auto lst = std::unique(first, last);
+    // returns distance between two iterators
     count = std::distance(first, lst);
     return count;
 }
